dc69827b7412bdb5d4e575c7f70845eec27c2dcd
sujithnixson <sujithnixson@gmail.com>
2024-06-19 19:24:15 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,75 +1,78 @@
-package com.example.todolistapp;
+package com.example.quoteofthedayapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AlertDialog;
-import android.content.DialogInterface;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    Button add;
-    AlertDialog dialog;
-    LinearLayout layout;
+
+    private TextView quoteTextView;
+    private Button refreshButton, shareButton, favoriteButton;
+
+    private String[] quotes = {
+            "The only way to do great work is to love what you do. - Steve Jobs",
+            "Believe you can and you're halfway there. - Theodore Roosevelt",
+            "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
+            // Add more quotes here
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        add=findViewById(R.id.add);
-        layout=findViewById(R.id.container);
+        quoteTextView = findViewById(R.id.quoteTextView);
+        refreshButton = findViewById(R.id.refreshButton);
+        shareButton = findViewById(R.id.shareButton);
+        favoriteButton = findViewById(R.id.favoriteButton);
 
-        buildDialog();
-        add.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                dialog.show();
-            }
-        });
-
-    }
-    public void buildDialog(){
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
-        View view = getLayoutInflater().inflate(R.layout.dialog, null);
-
-
-        final EditText name= view.findViewById(R.id.nameEdit);
-
-        builder.setView(view);
-        builder.setTitle("Enter your Task")
-                .setPositiveButton("SAVE",new DialogInterface.OnClickListener(){
-                    @Override
-                    public void onClick (DialogInterface dialog,int which){
-                        addCard(name.getText().toString());
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-
-        dialog = builder.create();
-    }
-    private void addCard(String name){
-        final View view = getLayoutInflater().inflate(R.layout.card,null);
-
-
-        TextView nameView = view.findViewById(R.id.name);
-        Button delete = view.findViewById(R.id.delete);
-        nameView.setText(name);
-        delete.setOnClickListener(new View.OnClickListener() {
+        refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                layout.removeView(view);
+                showRandomQuote();
             }
         });
-        layout.addView(view);
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareQuote();
+            }
+        });
+
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveToFavorites();
+            }
+        });
+
+        showRandomQuote();
     }
 
+    private void showRandomQuote() {
+        Random random = new Random();
+        int index = random.nextInt(quotes.length);
+        quoteTextView.setText(quotes[index]);
+    }
+
+    private void shareQuote() {
+        String currentQuote = quoteTextView.getText().toString();
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, currentQuote);
+
+        startActivity(Intent.createChooser(shareIntent, "Share Quote"));
+    }
+
+    private void saveToFavorites() {
+        String currentQuote = quoteTextView.getText().toString();
+
+    }
 }
